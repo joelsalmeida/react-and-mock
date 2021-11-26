97bbdb8bf48bc748a813604a4d3092f3a0ca2d99
Joel S. Almeida <joel.almeida.sa@gmail.com>
2021-11-26 19:34:17 -0300

    Test: Verifica se aparece o card com titulo de "Rick Sanchez"

--- react-and-mock-inicial/src/tests/App.test.js
+++ react-and-mock-inicial/src/tests/App.test.js
@@ -4,20 +4,20 @@ import App from '../App';
 import responseAPI from './mocks';
 
 describe('Test Rick & Morty API', () => {
+  beforeEach(() => {
+    global.fetch = jest.fn().mockResolvedValue({
+      json: jest.fn().mockResolvedValue(responseAPI),
+    });
 
-  beforeEach(()=>{
-   
-  })
+    render(<App />);
+  });
 
-  test('Verifica se aparece o card com titulo de "Rick Sanchez"',async () => {
-    
-  })
+  test('Verifica se aparece o card com titulo de "Rick Sanchez"', async () => {
+    const title = await screen.findByRole('heading', { name: /rick sanchez/i, level: 3 });
+    expect(title).toBeInTheDocument();
+  });
 
-  test('Verifica se existem o input de texto e o botão "Buscar"', () => {
-   
-  })
+  test('Verifica se existem o input de texto e o botão "Buscar"', () => {});
 
-  test('Verifica se ao buscar por "Smith" aparecem apenas 4 cards',async () => {
-  })
-
-})
+  test('Verifica se ao buscar por "Smith" aparecem apenas 4 cards', async () => {});
+});
