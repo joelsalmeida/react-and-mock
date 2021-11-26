0461b8665cea92a8069f51472459de7f02b9348d
Felipe <nunesfb@gmail.com>
2021-11-26 15:07:18 -0300

    react-and-mock-inicial

---
+++ react-and-mock-inicial/src/tests/App.test.js
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import responseAPI from './mocks';
+
+describe('Test Rick & Morty API', () => {
+
+  beforeEach(()=>{
+   
+  })
+
+  test('Verifica se aparece o card com titulo de "Rick Sanchez"',async () => {
+    
+  })
+
+  test('Verifica se existem o input de texto e o botão "Buscar"', () => {
+   
+  })
+
+  test('Verifica se ao buscar por "Smith" aparecem apenas 4 cards',async () => {
+  })
+
+})
